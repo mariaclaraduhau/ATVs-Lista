9c79ca59b9f4e8a8cdd304be9e9039e326fc2fb0
Maria Clara Gomes <duhau32@gmail.com>
2025-04-14 10:17:47 -0300

    Create Atividade03.cpp

---
+++ Atividade03.cpp
@@ -0,0 +1,190 @@
+#include<iostream>
+using namespace std;
+
+
+struct Node {
+
+    int value;
+    Node* next;
+
+    Node(int _value) {
+        value = _value;
+        next = NULL;
+    }
+
+};
+
+struct List {
+
+    Node* start;
+    Node* end;
+    int s;
+
+    List() {
+        start = NULL;
+        end = NULL;
+        s = 0;
+    }
+
+    // O(1)
+    bool empty() {
+        return start == NULL && end == NULL;
+    }
+
+    // O(1)
+    void pushBack(int value) {
+        Node* n = new Node(value);
+        s++;
+        if (empty()) {
+            start = n;
+            end = n;
+            return;
+        }
+        end->next = n;
+        end = n;
+    }
+
+    // O(1)
+    void pushFront(int value) {
+        Node* n = new Node(value);
+        s++;
+        if (empty()) {
+            start = n;
+            end = n;
+            return;
+        }
+        n->next = start;
+        start = n;
+    }
+
+    // O(n)
+    void print() {
+        Node* aux = start;
+        while (aux != NULL) {
+            printf("%d -> ", aux->value);
+            aux = aux->next;
+        }
+        printf("\n");
+    }
+
+    // O(n)
+    /*int size() {
+        int s = 0;
+        Node* aux = start;
+        while (aux != NULL) {
+            s++;
+            aux = aux->next;
+        }
+        return s;
+    }*/
+
+    // O(1)
+    int size() {
+        return s;
+    }
+
+    // O(1)
+    void popFront() {
+
+        if (empty()) return;
+
+        if (size() == 1) {
+            delete(start);
+            start = NULL;
+            end = NULL;
+            s = 0;
+            return;
+        }
+
+        Node *aux = start;
+        start = start->next;
+        delete(aux);
+        s--;
+
+    }
+
+    // O(n)
+    void popBack() {
+
+        if (empty()) return;
+
+        if (size() == 1) {
+            delete(start);
+            start = NULL;
+            end = NULL;
+            s = 0;
+            return;
+        }
+
+        Node *aux = start;
+        while (aux->next != end) {
+            aux = aux->next;
+        }
+
+        delete(end);
+        end = aux;
+        end->next = NULL;
+        s--;
+
+    }
+
+    //O(n)
+    void insert(int v, int p) {
+
+        if (p <= 0) {
+            pushFront(v);
+            return;
+        }
+
+        if (p >= s) {
+            pushBack(v);
+            return;
+        }
+
+        Node *n = new Node(v);
+        s++;
+
+        Node *a = start;
+        for (int i = 0; i < p - 1; i++) {
+            a = a->next;
+        }
+
+        n->next = a->next;
+        a->next = n;
+
+    }
+
+    void insertTamanho(){
+        pushBack(size());
+    }
+
+};
+
+int main() {
+
+    List l;
+    l.pushFront(10);
+    l.pushFront(3);
+    l.pushBack(3);
+    l.pushBack(5);
+    l.pushFront(8);
+    l.pushBack(50);
+    l.pushFront(78);
+    l.pushBack(1);
+    l.pushBack(75);
+    l.pushFront(25);
+    l.pushBack(60);
+    l.pushFront(34);
+    l.pushBack(80);
+    l.pushBack(30);
+    l.pushBack(23);
+    l.pushFront(5);
+    l.pushFront(66);
+    l.insert(5, 9);
+    l.print();
+    l.insertTamanho();
+    l.print();
+
+
+    return 0;
+}
